2853f7d53dd4d7f92c0d4d890a07428ead049e51
Souhaib Boujemaoui <souhaib.boujemaoui@mnd.thm.de>
2026-08-29 21:37:12 +0200

    feat: initialize spring boot application

---
+++ reportify/src/main/java/de/thm/reportify/ReportifyApplication.java
@@ -0,0 +1,13 @@
+package de.thm.reportify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportifyApplication.class, args);
+	}
+
+}
---
+++ reportify/src/test/java/de/thm/reportify/ReportifyApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thm.reportify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
